00aff3e40641adf19976d07b69ea10b7192824fe
student <student@K42-07>
2017-12-20 20:54:22 +0200

    Azure DB

---
+++ AzureSite/AzureSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AzureSite.MvcApplication" Language="C#" %>
---
+++ AzureSite/AzureSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureSite.Startup))]
+namespace AzureSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
